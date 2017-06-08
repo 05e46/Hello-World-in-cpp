dd3fb04a4f0a7928901b0812912d2303e38575cb
05e46 <phillip.perez44@gmail.com>
2017-06-08 16:18:42 -0700

    minor changes

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
 
+void input(string your_input);
+
 int main() {
 	cout << "Hello World" << endl;
-
+	string userInput;
+	cout << "Enter your input" << endl;
+	
 	system("pause");
 	return 0;
 }
